f91fb50dd9b5a3e7acb1ccfb6c542e6fb9bf9d2a
Mason Nesbitt <masonnesbitt@macbook-pro-10.local>
2018-10-23 10:21:20 -0500

    Initial Commit

---
+++ iOS Lyric Learner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS Lyric Learner
+//
+//  Created by Mason Nesbitt on 10/23/18.
+//  Copyright © 2018 Mason Nesbitt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
